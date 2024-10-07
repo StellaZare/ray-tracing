c7106f4bec109d48ec14cc84fa0349a18c5c41d8
Stella Zarei <zareistella@gmail.com>
2024-10-07 15:02:31 -0700

    Adds point/directional light direction() implementation

--- src/DirectionalLight.cpp
+++ src/DirectionalLight.cpp
@@ -1,11 +1,19 @@
 #include "DirectionalLight.h"
 #include <limits>
 
+/**
+ * Compute the direction to a direction-light 
+ * 
+ * @param q     query point in 3D space
+ * @param d     direction vector from query to direction-light
+ * @param mat_t parametric distance from q along d to reach light
+ */
 void DirectionalLight::direction(
-  const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
+    const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+    // (from query to light) = -(from light to query) 
+    d = -(this->d);
+    // light source is always infinite distance away
+    max_t = std::numeric_limits<double>
 }
 
--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -1,9 +1,19 @@
 #include "PointLight.h"
 
+/**
+ * Compute the direction to a point light source
+ * 
+ * @param q     query point in 3D space
+ * @param d     direction vector from q to the light
+ * @param max_t parametric distance from q to search
+ */
 void PointLight::direction(
-  const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
+    const Eigen::Vector3d & q, Eigen::Vector3d & d, double & max_t) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+    // Direction from query to point-light
+    d = (p - q);
+    // Distance from query to point-light
+    max_t = d.norm();
+    // Normalize direction vector
+    d.normalize();
 }
