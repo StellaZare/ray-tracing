662b0b6416344a8331c34823b3c94d1ff4a96c19
Stella Zarei <zareistella@gmail.com>
2024-10-09 16:40:44 -0700

    Adds reflected ray computation

--- src/reflect.cpp
+++ src/reflect.cpp
@@ -1,9 +1,15 @@
 #include <Eigen/Core>
 
+/**
+ * Reflect an incoming ray into an out going ray
+ * 
+ * @param in  incoming _unit_ ray direction
+ * @param n   surface _unit_ normal about which to reflect
+ * 
+ * @returns outward _unit_ ray direction of the reflected ray
+ */
 Eigen::Vector3d reflect(const Eigen::Vector3d & in, const Eigen::Vector3d & n)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return Eigen::Vector3d(0,0,0);
-  ////////////////////////////////////////////////////////////////////////////
+    Eigen::Vector3d reflected_ray = in - 2 * in.dot(n) * n;
+    return reflected_ray;
 }
